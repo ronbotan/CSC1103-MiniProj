43fcfef7380648e74b17a3dceed3313e3c46c85f
rontanziyu@gmail.com <rontanziyu@gmail.com>
2025-11-18 15:56:14 +0800

    Fixed merge pull issue

---
+++ gnuplot.h
@@ -0,0 +1,15 @@
+// gnuplot.h
+#ifndef GNUPLOT_H
+#define GNUPLOT_H
+
+#define ROW 3
+#define COLUMN 3
+
+extern char winner;
+extern int winLine[3];
+
+int  init_gnuplot(void);
+void close_gnuplot(void);
+void draw(char board[ROW][COLUMN], char winner, int winLine[3]);
+
+#endif
---
+++ single_player_knn.h
@@ -0,0 +1,6 @@
+#ifndef SINGLE_PLAYER_KNN_H
+#define SINGLE_PLAYER_KNN_H
+
+char Run1PKnn(char board[3][3], const char *dataset_path);
+
+#endif
